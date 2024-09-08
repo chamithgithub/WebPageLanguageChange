e5efa3cf009afad171a355d9cc102d3fb4c22c18
A.C.Dilshan <101160440+chamithgithub@users.noreply.github.com>
2024-09-09 00:08:52 +0530

    first step

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { useTranslation } from 'react-i18next';
+
+
+function App() {
+  const {t} = useTranslation()
+  return (
+    <div className="container">
+      <h1>{t('greeting')}</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/languageSelector.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const languageSelector = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default languageSelector
