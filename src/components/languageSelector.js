fb83c24c12932a972e15edbe7e67124efa62e5b9
A.C.Dilshan <101160440+chamithgithub@users.noreply.github.com>
2024-09-10 19:18:13 +0530

    update

--- src/components/languageSelector.js
+++ src/components/languageSelector.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+const languages = [
+  { code: "en", lang: "English" },
+  { code: "fr", lang: "French" },
+  { code: "hi", lang: "Hindi" },
+];
 
+handleLanguageChange = () => {
+
+  
+};
 const languageSelector = () => {
   return (
-    <div>
-      
+    <div className="btn-container">
+      {languages.map(({ lng }) => {
+        return (
+          <button
+            className="btn"
+            key={lng.code}
+            onClick={() => handleLanguageChange(code)}
+          >
+            {lang}
+          </button>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default languageSelector
+export default languageSelector;
